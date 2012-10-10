15ab7b2c9c4b68c75e47029ec25a99fecb26b99f
cbolt <cfajr@rocketmail.com>
2012-10-11 07:36:45 +0800

    another commit

--- Navigation/ViewController.m
+++ Navigation/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     // I'm adding code
+    // One more Add
 }
 
 - (void)didReceiveMemoryWarning
