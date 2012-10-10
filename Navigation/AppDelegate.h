750307c0aff2ce1ea8c22d6f8cd9c47e50f5dc8c
cbolt <cfajr@rocketmail.com>
2012-10-11 07:29:24 +0800

    Initial Commit

---
+++ Navigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Navigation
+//
+//  Created by Chito Cascante on 10/11/12.
+//  Copyright (c) 2012 prism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Navigation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Navigation
+//
+//  Created by Chito Cascante on 10/11/12.
+//  Copyright (c) 2012 prism. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
